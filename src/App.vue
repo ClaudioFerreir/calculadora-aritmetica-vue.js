58e7f74d4bb53bb476a562d3e36597610a87d71b
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-11-08 15:18:21 -0300

    feat: html components

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,35 @@
 </script>
 
 <template>
-  <h1>Ola Vue</h1>
+  <div class="container">
+    <header>
+      <h1 class="p-5 bg-light text-center mt-4 rounded">Calculadora Aritmética</h1>
+    </header>
+    <form>
+      <div class="row pt-4">
+        <div class="col">
+          <input type="number" placeholder="Digite um número" class="p-2 rounded-3 form-control">
+        </div>
+        <div class="col-md-1">
+          <select class="form-control text-center">
+            <option value="soma">+</option>
+            <option value="subtracao">-</option>
+            <option value="divisao">*</option>
+            <option value="multiplicacao">/</option>
+          </select>
+        </div>
+        <div class="col">
+          <input type="number" placeholder="Digite um número" class="p-2 rounded-3 form-control">
+        </div>
+      </div>
+    </form>
+    <div class="row pt-4 justify-content-center">
+      <p class="text-center fs-3">Solução</p>
+      <div class="col-4 text-center border p-3 rounded">
+        19 + 20 = 39
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
